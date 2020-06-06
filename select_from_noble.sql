3b6bfd7a900ed86f02223bcb845126a949e54fc6
Cathella <ncathie214@gmail.com>
2020-06-06 07:52:57 +0300

    Complete tutorial 3

--- select_from_noble.sql
+++ select_from_noble.sql
@@ -59,4 +59,17 @@ WHERE winner = 'PETER GRÜNBERG';
 /*Find all details of the prize won by EUGENE O'NEILL*/
 SELECT *
 FROM nobel
-WHERE winner = 'EUGENE O''NEILL'
+WHERE winner = 'EUGENE O''NEILL';
+
+/*List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.*/
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner;
+
+/*Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.*/
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY
+CASE WHEN subject IN('Chemistry','Physics')
+THEN 1 ELSE 0 END, subject, winner;
